774640585f8891276bb62c1c918c23ba60dd6939
quirin.niedernhuber <quirin.niedernhuber@gmail.com>
2025-09-05 08:37:25 +0000

    tweak stuffs

--- badgereader_addon/run.sh
+++ badgereader_addon/run.sh
@@ -4,21 +4,23 @@ bashio::log.info "NFC Badge Reader Addon starting..."
 
 bashio::log.info "Workdir $PWD"
 
-find . | bashio::log.info
+cat server.py
 
-find / | bashio::log.info
+cat server.py | bashio::log.info
 
+bashio::log.info "Hello1"
 
 # Start the Python HTTP server
 bashio::log.info "Starting Python HTTP server for badge messages..."
 python3 server.py
+bashio::log.info "Hello2"
 
 # If server.py exits, log it and keep container alive for debugging if needed,
 # though ideally server.py runs forever.
 bashio::log.warning "Python HTTP server exited."
-bashio::log.info "Container will sleep indefinitely to allow inspection if server exited unexpectedly."
+#bashio::log.info "Container will sleep indefinitely to allow inspection if server exited unexpectedly."
 
 # Loop forever to keep the container running if the python script exits
-while true; do
-  sleep 3600 # Sleep for an hour
-done
+#while true; do
+#  sleep 3600 # Sleep for an hour
+#done
